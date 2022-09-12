3ceb22c8b3d40a68f0062b6d1d89c64ba4cc6d75
unknown <nguyenhuy1323614@gmail.com>
2022-09-12 09:08:34 +0700

    second commit

---
+++ src/main/java/com/stress/demo/User.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.stress.demo;
+
+/**
+ *
+ * @author Huy
+ */
+public class User {
+    
+}
